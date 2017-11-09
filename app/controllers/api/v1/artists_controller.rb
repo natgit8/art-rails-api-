5afc7fa8ad2702b4445a08279d92b853bb0d8006
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-11-09 00:11:09 -0500

    API artists setup

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,9 +1,9 @@
-class ArtistsController < ApplicationController
+class Api::V1::ArtistsController < ApplicationController
 
   def index
     @artists = Artist.all
     render json: @artists
   end
 
-  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+artists = Artist.create( [{name: 'Andy Warhol', biography: 'Pop Art'}, { name: 'Jackson Pollock', biography: 'Abstract Expressionism'}])
+
+# artist1 = Artist.create(name: 'Andy Warhol', biography: 'Pop Art')
+#
+# artist2 = Artist.create(name: 'Jackson Pollock', biography: 'Abstract Expressionism')
